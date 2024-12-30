ed1b6f1813aaf38b2a3d345fb8c9ddd34381773f
Harsh <hjha0695@gmail.com>
2024-12-30 15:20:39 +0530

    Fix: added authorize header in swagger

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from app.routes import user_routes, auth_routes
+from fastapi.openapi.models import APIKey, APIKeyIn
+from fastapi.openapi.utils import get_openapi
+from app.routes import user_routes, auth_routes, post_routes
 
 app = FastAPI()
 
@@ -9,5 +11,30 @@ def read_root():
 
 app.include_router(user_routes.router, prefix="/users", tags=["Users"])
 app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
-# app.include_router(post_routes.router, prefix="/posts", tags=["Posts"])
+app.include_router(post_routes.router, prefix="/posts", tags=["Posts"])
 # app.include_router(follow_routes.router, prefix="/follow", tags=["Follow"])
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="InstaClone API",
+        version="1.0.0",
+        description="API documentation for InstaClone",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {
+        "BearerAuth": {
+            "type": "http",
+            "scheme": "bearer",
+            "bearerFormat": "JWT",
+        }
+    }
+    for path in openapi_schema["paths"].values():
+        for method in path.values():
+            method["security"] = [{"BearerAuth": []}]
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
